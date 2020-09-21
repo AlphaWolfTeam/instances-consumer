46904115bb8de0a9400b3e2cae8855550d384379
root <root@yoavrozner.wzpc1t2ka1fu3n1oktsqvzhcxh.bx.internal.cloudapp.net>
2020-09-21 15:10:26 +0000

    feat: init the consumer

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { DBHandler } from './utils/dbHandler';
+import { config } from './config';
+
+process.on("uncaughtException", console.error);
+
+(async () => {
+    try {
+	
+    } catch (error) {
+        console.error(error);
+    }
+})
